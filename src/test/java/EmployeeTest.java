78e3ef5cf715d1f44f938db6127fd301c721a5a7
Tomasz Cichosz <tomaszcichosz@gmail.com>
2018-07-04 19:07:36 +0200

    refactor EmployeeTest

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,39 +1,40 @@
 import model.ContractEmployee;
 import model.HourlyEmployee;
 import model.SalaryEmployee;
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class EmployeeTest {
 
     @Test
-    public void givenHourlyEmployee_whenCalculatePaycheckMethod_thenEquals() {
+    public void givenHourlyEmployee_whenCalculatePaycheckMethod_thenPaycheckValueCalculatedCorrectly() {
         HourlyEmployee hourlyEmployee = new HourlyEmployee();
         hourlyEmployee.setHoursWorked(100);
         hourlyEmployee.setHourlyCost(2d);
 
         double paycheck = hourlyEmployee.calculatePaycheck();
 
-        Assert.assertEquals(200d, paycheck, 0.0001d);
+        assertEquals(200d, paycheck, 0.0001d);
     }
 
     @Test
-    public void givenContractEmployee_whenCalculatePaycheckMethod_thenEquals() {
+    public void givenContractEmployee_whenCalculatePaycheckMethod_thenPaycheckValueCalculatedCorrectly() {
         ContractEmployee contractEmployee = new ContractEmployee();
         contractEmployee.setContractCost(100);
 
         double paycheck = contractEmployee.calculatePaycheck();
 
-        Assert.assertEquals(85d, paycheck, 0.0001d);
+        assertEquals(85d, paycheck, 0.0001d);
     }
 
     @Test
-    public void givenSalaryEmployee_whenCalculatePaycheckMethod_thenEquals() {
+    public void givenSalaryEmployee_whenCalculatePaycheckMethod_thenPaycheckValueCalculatedCorrectly() {
         SalaryEmployee salaryEmployee = new SalaryEmployee();
         salaryEmployee.setMonthlySalary(1000);
 
         double paycheck = salaryEmployee.calculatePaycheck();
 
-        Assert.assertEquals(770d, paycheck, 0.0001d);
+        assertEquals(770d, paycheck, 0.0001d);
     }
 }
